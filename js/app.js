3e3ee654ebc11b952576aa160c11a861b0d70ea3
Kelsey Smith <845872897@bun2-206-12.local>
2018-01-24 14:48:37 +1300

    fixed

--- js/app.js
+++ js/app.js
@@ -6,42 +6,63 @@ function getData() {
         method: "GET",
         dataType: "json",
         url: "./js/classData.json",
-        success: function(data) {
-            console.log(data);
-            var tableData = new google.visualization.DataTable();
-            tableData.addColumn('number', 'Age');
-            tableData.addColumn('string', 'Gender');
-            tableData.addColumn('string','Eye Colour');
-            tableData.addColumn('number', 'Height');
-            tableData.addColumn('boolean', 'Car Ownership');
-            tableData.addColumn('number', 'Hours Of Work');
-            tableData.addColumn('string', 'Socail Media');
+        success: function(dataFromJSON) {
+            console.log(dataFromJSON);
+            var data = new google.visualization.DataTable();
+            data.addColumn('number', 'Age');
+            data.addColumn('string', 'Gender');
+            data.addColumn('string','Eye Colour');
+            data.addColumn('number', 'Height');
+            data.addColumn('boolean', 'Car Ownership');
+            data.addColumn('number', 'Hours Of Work');
+            data.addColumn('string', 'Social Media');
 
-            for (var i = 0; i < data.length; i++) {
-                tableData.addRow([
-                    data[i].age,
-                    data[i].gender,
-                    data[i].eyeColor,
-                    data[i].height,
-                    data[i].ownsCar,
-                    data[i].workingHoursPerWeek,
-                    data[i].preferredSocialMedia
+            for (var i = 0; i < dataFromJSON.length; i++) {
+                data.addRow([
+                    dataFromJSON[i].age,
+                    dataFromJSON[i].gender,
+                    dataFromJSON[i].eyeColor,
+                    dataFromJSON[i].height,
+                    dataFromJSON[i].ownsCar,
+                    dataFromJSON[i].workingHoursPerWeek,
+                    dataFromJSON[i].preferredSocialMedia
                     ]);
             };
 
-        var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard'));
+        var dashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard'));
 
         var scatterChart = new google.visualization.ChartWrapper({
             chartType: 'ScatterChart',
             containerId: 'scatterChart',
             options:{
                 title: 'Age Vs. Height',
-                legend: 'none'
+                legend: 'none',
+                width: '100%',
+                height: '100%'
             },
             view:{
                 columns: [0, 3]
             }
-        })
+        });
+
+      var genderPicker = new google.visualization.ControlWrapper({
+
+                controlType: 'CategoryFilter',
+                containerId: 'genderSelect',
+                options:{
+                        filterColumnLabel: 'Gender',
+                        ui:{
+                            allowMultiple: false,
+                            allowTyping: false,
+                            labelStacking:'vertical'
+                        }
+                
+                }
+            });
+        dashboard.bind([genderPicker], [scatterChart]);
+        dashboard.draw(data);
+
         },
         error: function(response) {
             console.log("Error Code: " + response.status + "\n" + response.statusText);
